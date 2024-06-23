52f143bdaebb3de9d66dfb332079f1013c53d935
andikashln <andikashalihin01@gmail.com>
2024-06-23 19:34:22 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    
+    <h2>
+      Andika Shalihin<br>
+      <br>  
+      <h3>Ini merupakan tugas portfolio untuk memenuhi tugas Prak PBK</h3>
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+  padding: 5%;
+}
+
+h2 {
+  font-weight: 500;
+  font-size:180%;
+  position: relative;
+  top: -10px;
+  color: white;
+  padding: 5%;
+}
+
+h3 {
+  font-size: 1rem;
+  font-weight: 400;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,58 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Latar Pendidikan</template>
+
+    Saya merupakan mahasiswa Universitas Islam Riau, sebelumnya saya pernah bersekolah di SMA 14 Pekanbaru, MTsN 3 Pekanbaru, dan SD 21 Pekanbaru.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Pengalaman dan Keahlian</template>
+
+    Bermain futsal dan bermain game 
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Karya</template>
+
+    Pada sosial media saya menjadi streaming game, dari situ saya bisa berjual untuk memancing viewers yang nonton untuk belanja di toko yang saya sediakan
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Sertifikasi</template>
+
+    Saya pernah mengambil beberapa sertifikasi pada PT tertentu
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Kontak</template>
+
+   Hubungi saya melalui:<br>
+   <li>Email: andikashalihin@gmail.com</li>
+   <li>Instagram: @andikashln</li>
+  </WelcomeItem>
+</template>
